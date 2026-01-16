7d4bbf613e6ab3413b9ef2e1c10e89ec6edca783
Karen Blanco <kapaucastillo@gmail.com>
2026-01-16 10:45:04 -0600

    primer pantalla

--- CentroMedico/viewers/PrincipalViewer.xaml.cs
+++ CentroMedico/viewers/PrincipalViewer.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Linq; 
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,17 +12,67 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using CentroMedico.Database;
+using CentroMedico.models;
+using Microsoft.EntityFrameworkCore;
 
 namespace CentroMedico.viewers
 {
-    /// <summary>
-    /// Lógica de interacción para PrincipalViewer.xaml
-    /// </summary>
-    public partial class PrincipalViewer : Page
+
+    public partial class principalViewer : UserControl
     {
-        public PrincipalViewer()
+     
+        private List<patientModel> _todosLosPacientes;
+
+        public principalViewer()
         {
             InitializeComponent();
+
+            ConsultorioContext.PrepararBaseDeDatos();
+
+            CargarDatos();
         }
+
+        private void CargarDatos()
+        {
+            try
+            {
+                using (var db = new ConsultorioContext())
+                {
+     
+                    _todosLosPacientes = db.Patients.ToList();
+                }
+
+                listaPacientes.ItemsSource = _todosLosPacientes;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al cargar datos: {ex.Message}");
+            }
+        }
+
+        private void txtBuscar_TextChanged(object sender, TextChangedEventArgs e)
+        {
+  
+            if (_todosLosPacientes == null) return;
+
+
+            string textoBusqueda = txtBuscar.Text.ToLower();
+
+
+            if (string.IsNullOrEmpty(textoBusqueda))
+            {
+                listaPacientes.ItemsSource = _todosLosPacientes;
+                return;
+            }
+
+            var listaFiltrada = _todosLosPacientes
+                                .Where(paciente => paciente.name != null &&
+                                                   paciente.name.ToLower().Contains(textoBusqueda))
+                                .ToList();
+
+            listaPacientes.ItemsSource = listaFiltrada;
+        }
+
     }
 }
